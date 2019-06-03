b88fc3ce9c4f09d5eaadd760b933b5831a7a48b1
renzhendong <rzdong163@163.com>
2019-06-03 22:25:42 +0800

    侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-        <a-button type="primary" class=""><a-icon type="step-backward" />rewqrewq</a-button>
-                <router-view/>
+        <a-layout style="height: 100%">
+            <a-layout-sider theme="light" :width="250" :style="{'overflow': 'hidden'}">
+                <div class="logo">
+
+                </div>
+                <a-menu theme="light"
+                    mode="inline"
+                    :defaultSelectedKeys="['1']"
+                    :defaultOpenKeys="['sub1']"
+                >
+                    <a-sub-menu key="sub1">
+                        <span slot="title"><a-icon type="user" />subnav 1</span>
+                        <a-menu-item key="1">主页1</a-menu-item>
+                        <a-menu-item key="2">主页1</a-menu-item>
+                        <a-menu-item key="3">主页1</a-menu-item>
+                        <a-menu-item key="4">主页1</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub2">
+                        <span slot="title"><a-icon type="user" />subnav 1</span>
+                        <a-menu-item key="5">主页1</a-menu-item>
+                        <a-menu-item key="6">主页1</a-menu-item>
+                        <a-menu-item key="7">主页1</a-menu-item>
+                        <a-menu-item key="8">主页1</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub3">
+                        <span slot="title"><a-icon type="user" />subnav 1</span>
+                        <a-menu-item key="9">主页1</a-menu-item>
+                        <a-menu-item key="10">主页1</a-menu-item>
+                        <a-menu-item key="11">主页1</a-menu-item>
+                        <a-menu-item key="12">主页1</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub4">
+                        <span slot="title"><a-icon type="user" />subnav 1</span>
+                        <a-menu-item key="13">主页1</a-menu-item>
+                        <a-menu-item key="14">主页1</a-menu-item>
+                        <a-menu-item key="15">主页1</a-menu-item>
+                        <a-menu-item key="16">主页1</a-menu-item>
+                    </a-sub-menu>
+                </a-menu>
+
+            </a-layout-sider>
+            <a-layout>
+                <a-layout-header>Header</a-layout-header>
+                <a-layout-content>Content</a-layout-content>
+                <a-layout-footer>Footer</a-layout-footer>
+            </a-layout>
+        </a-layout>
     </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import {Button} from 'ant-design-vue';
-// const Button = require('ant-design-vue/lib/button');
-import Button from 'ant-design-vue/lib/button'
-const Icon = require('ant-design-vue/lib/icon')
 @Component({
     components: {
-        aButton: Button,
-        aIcon: Icon,
     }
 })
 export default class App extends Vue {
@@ -29,12 +64,28 @@ export default class App extends Vue {
 
 
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+body {
+    overflow-x: hidden;
+}
+
+.ant-layout-sider-children {
+    width: 264px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+}
+
+.ant-menu-submenu {
+    border-bottom: 1px solid #eee;
+}
+
 #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    width: 100%;
+    height: 100%;
     color: #2c3e50;
     transform: translate(1px, 2px);
 }
