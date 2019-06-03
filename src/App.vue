702ff2c2570d4cb7e7f2f959f3e40e1749afdd95
renzhendong <rzdong163@163.com>
2019-06-03 23:30:41 +0800

    菜单初始化切换

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,24 @@
         <a-layout style="height: 100%">
             <a-layout-sider theme="light" :width="250" :style="{'overflow': 'hidden'}">
                 <div class="logo">
-
+                    <a-row class="title"> 
+                        <a-col :span="24" class="title24">
+                            <a-icon type="radar-chart" /> XX管理系统
+                        </a-col>
+                    </a-row>
+                    <a-row class="userinfo">
+                        <a-col :span="8">
+                            <img src="./assets/avatar.jpg" alt="">
+                        </a-col>
+                        <a-col :span="16">
+                            <div class="username">用户名字</div>
+                        </a-col>
+                    </a-row>
                 </div>
                 <a-menu theme="light"
                     mode="inline"
-                    :defaultSelectedKeys="['1']"
-                    :defaultOpenKeys="['sub1']"
+                    :defaultSelectedKeys="defaultSelectedKeys"
+                    :defaultOpenKeys="defaultOpenKeys"
                 >
                     <a-sub-menu key="sub1">
                         <span slot="title"><a-icon type="user" />subnav 1</span>
@@ -57,8 +69,18 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class App extends Vue {
 
+    private defaultSelectedKeys: any[] = ['1']
+    private defaultOpenKeys: any[] = ['sub1']
 
+    private mounted(): void {
+        setTimeout(() => {
+            this.defaultSelectedKeys.pop();
+            this.defaultSelectedKeys.push('5');
 
+            this.defaultOpenKeys.pop()
+            this.defaultOpenKeys.push('sub2')
+        }, 1500);
+    }
 }
 </script>
 
@@ -96,6 +118,36 @@ body {
         color: #2c3e50;
         &.router-link-exact-active {
             color: #42b983;
+        }
+    }
+}
+
+.logo {
+    width: 100%;
+    height: 154px;
+    border-bottom: 1px solid #eee;
+    .title {
+        height: 64px;
+        width: 100%;
+        border-bottom: 1px solid #eee;
+        border-right: 1px solid #eee;
+        .title24 {
+            line-height:64px;
+            padding: 0 20px;
+            font-size: 22px;
+        }
+    }
+    .userinfo {
+        height: 90px;
+        padding: 10px;
+        img {
+            width: 70px;
+            height: 70px;
+            border-radius: 50%;
+        }
+        .username {
+            line-height: 70px;
+            padding-left: 15px;
         }
     }
 }
